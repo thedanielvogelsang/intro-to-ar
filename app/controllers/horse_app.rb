93158b448a788089d4590a1f458dc05611622c84
thedanielvogelsang <dr.vogelsang26@gmail.com>
2017-06-29 10:35:26 -0600

    fully functional

--- app/controllers/horse_app.rb
+++ app/controllers/horse_app.rb
@@ -1,6 +1,30 @@
 class HorseApp < Sinatra::Base
+  set :method_overide, true
+
   get '/horses' do
     @horses = Horse.all
     erb :"horses/index"
+  end
+
+  get '/horses/:id' do
+    @horse = Horse.find(params[:id])
+    erb :"horses/show"
+  end
+
+  get '/horses/:id/edit' do
+    @horse = Horse.find(params[:id])
+    erb :"horses/edit"
+  end
+
+  put '/horses/:id' do |id|
+    Horse.update(id.to_i, params[:horse])
+    redirect "/horses/#{id}"
+  end
+
+  get '/jockey/:id' do |id|
+    @jockey = Jockey.find(id)
+    @total_winnings = Horse.total_winnings?(@jockey.id)
+    require 'pry' ; binding.pry
+    erb :"jockeys/show"
   end
 end
--- app/models/horse.rb
+++ app/models/horse.rb
@@ -1,3 +1,8 @@
 class Horse < ActiveRecord::Base
-  
+  belongs_to :jockey
+  belongs_to :breed
+
+  def self.total_winnings?(jockey_id)
+    Horse.where(jockey_id: jockey_id).sum(:total_winnings)
+  end
 end
